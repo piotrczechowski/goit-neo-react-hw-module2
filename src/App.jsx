d4cf59cc3a3ea87aa9f8f3fb0b6140ba51fa4b1f
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2025-02-09 16:54:53 +0100

    add label no feedback was given

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ function App() {
 const [good, setGood] = useState(0);
 const [neutral, setNeutral] = useState(0);
 const [bad, setBad] = useState(0);
-const [total, setTotal] = useState(0);
+// const [total, setTotal] = useState(0);
 
 const handleGood = () => {setGood(good + 1)};
 const handleNeutral = () => {setNeutral(neutral + 1)};
 const handleBad = () => {setBad(bad + 1)};
 const handleReset = () => {setGood(0); setNeutral(0); setBad(0)};
-const handleTotal = () => {setTotal(good + neutral + bad)};
+const total = good + neutral + bad;
   
 return (
     <>
--- src/Feedback.jsx
+++ src/Feedback.jsx
@@ -1,5 +1,7 @@
 export default function Feedback({ good, neutral, bad, total }) {
-    
+    if (total === 0) {
+        return <p>No feedback given</p>
+    }
     
     return (
       <>
