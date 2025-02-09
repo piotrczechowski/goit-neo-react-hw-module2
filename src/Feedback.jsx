f5dbf63ea8be1a5eadda84eadc9ec9bb02130426
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2025-02-09 16:52:01 +0100

    fix total

--- src/Feedback.jsx
+++ src/Feedback.jsx
@@ -1,6 +1,9 @@
-export default function Feedback({ good, neutral, bad }) {
+export default function Feedback({ good, neutral, bad, total }) {
+    
+    
     return (
       <>
+      
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
